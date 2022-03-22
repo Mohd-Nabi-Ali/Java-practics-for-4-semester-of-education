621c47e2a4959f52f3af7fdccdd53da4754c3cf4
Mohd Nabi Ali <83170926+Mohd-Nabi-Ali@users.noreply.github.com>
2022-03-22 15:50:16 +0300

    Add files via upload

---
+++ prac10/HarryPotter.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+public class HarryPotter implements Magican {
+    @Override
+    public void doMagic() {
+        System.out.println("Harry Potter do magic ...");
+    }
+}
---
+++ prac10/Voldemort.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+public class Voldemort implements Magican{
+    @Override
+    public void doMagic() {
+        System.out.println("Voldemort do magic ...");
+    }
+}
