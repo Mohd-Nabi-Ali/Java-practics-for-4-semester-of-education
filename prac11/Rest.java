12d74276f77ec83d5c261f4cfe4e2f2ef7f7e3de
Mohd Nabi Ali <83170926+Mohd-Nabi-Ali@users.noreply.github.com>
2022-03-22 17:35:16 +0300

    Add files via upload

---
+++ prac11/Rest.java
@@ -0,0 +1,14 @@
+package com.example.demo1;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+    @RestController
+    class Rest {
+        @RequestMapping("/actuator")
+        public @ResponseBody String rest(){
+            return "яяяяяя";
+        }
+    }
+
